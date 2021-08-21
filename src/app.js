a813b2fc9e04b7de71e4082f685a79556b5703cc
Alex Finkel <alex.finkel@stileeducation.com>
2021-08-21 15:55:18 +1000

    Demo adding 2 giraffes.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,39 @@
 //Create a Pixi Application
-const App = new PIXI.Application({ width: 256, height: 256 });
-export default App;
+export const App = new PIXI.Application({ width: 512, height: 512 });
+
+export class Giraffe {
+    static LoadPromise = null;
+    static async Load() {
+        if (this.LoadPromise !== null) {
+            return this.LoadPromise;
+        }
+        this.LoadPromise = new Promise((resolve, reject) => {
+            PIXI.Loader.shared
+                .add([
+                    "images/giraffe-whole.png",
+                    "images/giraffe-legs.png",
+                    "images/giraffe-head.png",
+                    "images/giraffe-neck.png",
+                ])
+                .load(resolve);
+        });
+        return this.LoadPromise;
+    }
+    constructor(stage) {
+        this.stage = stage;
+    }
+    show(x, y) {
+        const sprite = new PIXI.Sprite(
+            PIXI.Loader.shared.resources["images/giraffe-whole.png"].texture
+        );
+        sprite.x = x;
+        sprite.y = y;
+        sprite.width = 32;
+        sprite.height = 32;
+        this.stage.addChild(sprite);
+    }
+}
+
+await Giraffe.Load();
+const g1 = new Giraffe(App.stage);
+g1.show(App.screen.width / 4, App.screen.height / 4);
